93ce75ab16ddc21cd3792392d59dd9f35f8e3c0d
Jhonatan Padua Soares <61254467+jhonatanps@users.noreply.github.com>
2023-11-09 18:58:51 -0300

    versel_teste2

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const routes = require('./src/routes');
+const routes = require('./routes');
 require('dotenv').config();
 
 const app = express();
